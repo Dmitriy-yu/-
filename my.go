56fcba56a468fa41106d1666428b5aad885fc573
Dmitriy Yurchenko <97550947+Dmitriy-yu@users.noreply.github.com>
2022-07-14 17:51:28 +0300

    Добавил функционал для оператора деление!

--- my.go
+++ my.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	
 )
 func main(){
 var value1 int
 var value2 int 
 var result string
+for i:=0;;i++{
 fmt.Println("Введите первое число") 
 fmt.Scan(&value1)
 fmt.Println("Введите второе число")
@@ -19,9 +21,17 @@ fmt.Println(value1+value2)
 case  "-":
 	fmt.Println(value1-value2)
 case  "*":
-	fmt.Println(value1*value2)
-case  " /":
-	fmt.Println(value1/value2)
+      fmt.Println(value1*value2)
+	
+case  "/":
+	if value1==0||value2==0 {
+		fmt.Println("На ноль делить нельзя")
+		
+	} 
+	if  value1>0&&value2>0{
+		fmt.Println(value1/value2)
+	}
+}
 }
 
 }	
