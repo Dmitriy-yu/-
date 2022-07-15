2d8eea27e13cff3fbb8b36685d96c218c4b066d1
Dmitriy Yurchenko <97550947+Dmitriy-yu@users.noreply.github.com>
2022-07-15 16:35:29 +0300

    Добавил обработчик

--- train.go
+++ train.go
@@ -1,24 +1,24 @@
 package main
 import "fmt"
 type Person struct{
-	num1 int
-	num2 int
+	num1 float64
+	num2 float64
 }
-func (p Person)plus()int{
+func (p Person)plus()float64{
 	return p.num1+p.num2
 }
-func (p Person)minus()int{
+func (p Person)minus()float64{
 	return p.num1-p.num2
 }
-func (p Person)divide()int{
+func (p Person)divide()float64{
 	return p.num1/p.num2
 }
-func (p Person)multiply()int{
+func (p Person)multiply()float64{
 	return p.num1*p.num2
 }
 func main(){
-var a int
-var b int
+var a float64
+var b float64
 var c string
 for i:=0;;i++{
 fmt.Println("Введите первое число")
@@ -33,8 +33,11 @@ if c=="+" {
 }else if c=="-"{
 	fmt.Println("Результат равен",result.minus())
 }else if c=="/"{
-	fmt.Println("Результат равен",result.divide())
-}else if c=="*"{
+	if a==0||b==0 {
+		fmt.Println("На ноль делить нельзя")
+	}
+	fmt.Println("Результат равен",float64(result.divide()))
+    }else if c=="*"{
 	fmt.Println("Результат равен",result.multiply())
 }
 }
